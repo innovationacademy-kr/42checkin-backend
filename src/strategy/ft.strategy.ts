900329512d23cc59cb4aaa4117d7ce2cdfc3f85e
padawanR0k <padawanR0k@gmail.com>
2021-06-10 01:05:01 +0900

    add tracer logger

--- src/strategy/ft.strategy.ts
+++ src/strategy/ft.strategy.ts
@@ -1,11 +1,15 @@
 import config from '@config/configuration';
 import User from '@entities/user.entity';
 import passport from 'passport';
+import { MyLogger } from '../service/logger.service';
 var FortyTwoStrategy = require('passport-42').Strategy;
 
 const validate = (token: string, rt: string, profile: any) => {
+	const logger = new MyLogger();
 	try {
+		logger.debug('oauth validation start');
 		const user = new User(profile.id, profile.username, profile.emails[0].value);
+		logger.debug('authroized info : ', profile.id, profile.username);
 		if (profile._json.cursus_users.length < 2) {
 			// throw new NotAcceptableException();
 			throw Error('NotAcceptableException');
@@ -13,6 +17,7 @@ const validate = (token: string, rt: string, profile: any) => {
 			return user;
 		}
 	} catch (e) {
+		logger.info(e);
 		return false;
 	}
 };
