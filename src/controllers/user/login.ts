ddbd411b55eceb399d81a34acfb679aae715820e
padawanR0k <padawanR0k@gmail.com>
2021-06-30 16:47:53 +0900

    CHKN-34 로그인 리다이렉트 URL 변경

--- src/controllers/user/login.ts
+++ src/controllers/user/login.ts
@@ -34,7 +34,7 @@ export default class Login extends BaseRoute {
 	 * 42API 로그인 후 리다이렉트 되는 엔드포인트입니다.
 	 * 42API에서 유저 정보를 가져와 JWT 토큰을 발행합니다.
 	 * JWT토큰을 쿠키에 w_auth로 담아 전송합니다.
-	 * /submit 페이지로 리다이렉트 합니다.
+	 * /checkin 페이지로 리다이렉트 합니다.
 	 * @param req
 	 * @param res
 	 * @param next
@@ -51,7 +51,7 @@ export default class Login extends BaseRoute {
 				cookieOption.domain = '.42seoul.io'
 			}
 			res.cookie('w_auth', token, cookieOption);
-			res.status(302).redirect(this.redirectUrlOrigin + '/submit');
+			res.status(302).redirect(this.redirectUrlOrigin + '/checkin');
 		} else {
 			res.status(403).json({ result: false });
 		}
