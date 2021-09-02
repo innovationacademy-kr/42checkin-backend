f036865181826b63c2e5beab6db22a5ede4db0ef
padawanR0k <padawanR0k@gmail.com>
2021-09-02 14:14:37 +0900

    CHKN-64 log api 페이지네이션에 아이템갯수지정 쿼리 추가

--- src/service/log.service.ts
+++ src/service/log.service.ts
@@ -11,7 +11,7 @@ import Sequelize from 'sequelize';
 /**
  * 유저의 로그정보를 조회한다.
  */
-export const getUserLog = async (login: string, page: number) => {
+export const getUserLog = async (login: string, page: number, listSize: number) => {
 	logger.info('userName: ', login);
 	const { rows, count } = await DB.log.findAndCountAll({
 		include: [
@@ -28,16 +28,16 @@ export const getUserLog = async (login: string, page: number) => {
 			}
 		],
 		order: [ [ 'createdAt', 'DESC' ] ],
-		offset: 50 * (page - 1),
-		limit: 50
+		offset: listSize * (page - 1),
+		limit: listSize
 	});
-	return { list: rows, lastPage: Math.ceil(count / 50) };
+	return { list: rows, lastPage: Math.ceil(count / listSize) };
 };
 
 /**
  * 카드의 로그정보를 조회한다.
  */
-export const getCardLog = async (id: number, page: number) => {
+export const getCardLog = async (id: number, page: number, listSize: number) => {
 	logger.info('cardId: ', id);
 	const { rows, count } = await DB.log.findAndCountAll({
 		include: [
@@ -54,10 +54,10 @@ export const getCardLog = async (id: number, page: number) => {
 			}
 		],
 		order: [ [ 'createdAt', 'DESC' ] ],
-		offset: 50 * (page - 1),
-		limit: 50
+		offset: listSize * (page - 1),
+		limit: listSize
 	});
-	return { list: rows, lastPage: Math.ceil(count / 50) };
+	return { list: rows, lastPage: Math.ceil(count / listSize) };
 };
 
 /**
@@ -72,7 +72,7 @@ export const createLog = async (user: UserModel, card: CardModel, type: string):
 /**
  * 클러스터별 로그정보를 조회한다.
  */
-export const getCluster = async (type: CLUSTER_CODE, page: number) => {
+export const getCluster = async (type: CLUSTER_CODE, page: number, listSize: number) => {
 	if (!CLUSTER_CODE[type]) throw new ApiError(httpStatus.NOT_FOUND, '존재하지 않는 클러스터 코드입니다.');
 	logger.info(`get ${CLUSTER_CODE[type]} cluster info (page: ${page})`);
 	const { rows, count } = await DB.log.findAndCountAll({
@@ -90,16 +90,16 @@ export const getCluster = async (type: CLUSTER_CODE, page: number) => {
 			}
 		],
 		order: [ [ 'createdAt', 'DESC' ] ],
-		offset: 50 * (page - 1),
-		limit: 50
+		offset: listSize * (page - 1),
+		limit: listSize
 	});
-	return { list: rows, lastPage: Math.ceil(count / 50) };
+	return { list: rows, lastPage: Math.ceil(count / listSize) };
 };
 
 /**
  * 특정 클러스터의 미반납카드를 조회한다.
  */
-export const getCheckIn = async (clusterType: number, page: number) => {
+export const getCheckIn = async (clusterType: number, page: number, listSize: number) => {
 	if (!CLUSTER_CODE[clusterType]) throw new ApiError(httpStatus.NOT_FOUND, '존재하지 않는 클러스터 코드입니다.');
 	logger.info(`getChekcin type: ${CLUSTER_CODE[clusterType]}`);
 	const { rows, count } = await DB.log.findAndCountAll({
@@ -111,16 +111,16 @@ export const getCheckIn = async (clusterType: number, page: number) => {
 			]
 		},
 		order: [ [ 'createdAt', 'DESC' ] ],
-		offset: 50 * (page - 1),
-		limit: 50
+		offset: listSize * (page - 1),
+		limit: listSize
 	});
-	return { list: rows, lastPage: Math.ceil(count / 50) };
+	return { list: rows, lastPage: Math.ceil(count / listSize) };
 };
 
 /**
  * 특정 클러스터의 로그정보를 조회한다.
  */
-export const getAllCard = async (clusterType: number, page: number) => {
+export const getAllCard = async (clusterType: number, page: number, listSize: number) => {
 	if (!CLUSTER_CODE[clusterType]) throw new ApiError(httpStatus.NOT_FOUND, '존재하지 않는 클러스터 코드입니다.');
 	logger.info(`getAllcard type: ${CLUSTER_CODE[clusterType]}`);
 
@@ -133,8 +133,8 @@ export const getAllCard = async (clusterType: number, page: number) => {
 			]
 		},
 		order: [ [ 'user', 'cardId', 'DESC' ] ],
-		offset: 50 * (page - 1),
-		limit: 50
+		offset: listSize * (page - 1),
+		limit: listSize
 	});
-	return { list: rows, lastPage: Math.ceil(count / 50) };
+	return { list: rows, lastPage: Math.ceil(count / listSize) };
 };
