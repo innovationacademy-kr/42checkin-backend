b8a80f0181f64ec47447e5670210529ea255faba
padawanR0k <padawanR0k@gmail.com>
2021-09-10 22:20:29 +0900

    CHKN-66 WiFi ip필터 코드 개선

--- src/middlewares/ipFilter.ts
+++ src/middlewares/ipFilter.ts
@@ -1,24 +1,32 @@
 import config from '@config/configuration';
 import logger from '@lib/logger';
 import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import ApiError from '../lib/errorHandle';
 
-export const ipFilter = (ips: string[]) => async (req: Request, res: Response, next: NextFunction) => {
+const ipFilter = (rules: Function[]) => async (req: Request, res: Response, next: NextFunction) => {
 	const { clientIp } = req;
-	if (config.env === 'development') {
-		ips.push("*.*.*.*");
-	} else if (config.env === 'test') {
-		ips.push(config.ip.developer01);
-	} else if (config.env === 'production') {
-		ips.push(config.ip.developer01);
-	}
 
-	console.log({clientIp});
-	if (!ips.includes(clientIp)) {
+	if (!rules.every(rule => rule())) {
 		logger.info({ clientIp });
-		res.status(401).json({
-			message: '42seoul Guest WiFi를 사용해주세요.'
-		});
+		throw new ApiError(httpStatus.UNAUTHORIZED,  '42seoul Guest WiFi를 사용해주세요.')
 	}
 
 	next();
 };
+
+const checkIsAdmin = (ip: string) => {
+	const ips = [config.ip.developer01]
+	return ips.includes(ip)
+}
+
+/** 로드밸런서 IP 범위 */
+const isGuestWiFi = (ip: string) => {
+	const ips = [config.ip.guest]
+	return ips.includes(ip)
+}
+
+export const GuestWiFiIpFilter = () => {
+	const rules: Function[] = [checkIsAdmin, isGuestWiFi];
+	return ipFilter(rules);
+}
--- src/routes/user/check.ts
+++ src/routes/user/check.ts
@@ -2,14 +2,12 @@ import passport from 'passport';
 import { JwtStrategy } from '@strategy/jwt.strategy';
 import { Router } from 'express';
 import * as checkController from '@controllers/user/check';
-import { ipFilter } from 'src/middlewares/ipFilter';
-import config from '@config/configuration';
+import { GuestWiFiIpFilter } from 'src/middlewares/ipFilter';
 
-const ips = [config.ip.guest];
 export const path = '/';
 export const router = Router();
 
 passport.use(JwtStrategy());
-router.use(ipFilter(ips));
+router.use(GuestWiFiIpFilter());
 router.post('/checkIn/:cardid', passport.authenticate('jwt'), checkController.checkIn);
 router.post('/checkOut', passport.authenticate('jwt'), checkController.checkOut);
